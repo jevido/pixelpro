2196bfe6b3e164b7599b9684647a14e94ce80e48
jevido <jheremenis@gmail.com>
2021-04-20 00:43:12 +0200

    added unexpected anonymous upload

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,7 +1,8 @@
 <template>
 <div class="border-dashed border-2 w-full h-32 rounded"
-	@mouseover="hover = true"
-	@mouseleave="leaveDropZone"
+	@dragover.prevent="enterDropZone"
+	@dragleave.prevent="leaveDropZone"
+  @drop="onDrop"
 	:class="hover ? colorClass : 'border-gray-300'"
 >
   <div class="m-2 block text-grey p-10 text-center">Drop your files here</div>
@@ -9,6 +10,8 @@
 </template>
 
 <script>
+const { ipcRenderer } = require('electron');
+
 export default {
   data() {
     return {
@@ -17,12 +20,21 @@ export default {
     };
   },
 	methods: {
+    onDrop: function(ev) {
+      this.hover = false;
+      for (let file of ev.dataTransfer.files) {
+        ipcRenderer.send('upload', file.path)
+      }
+    },
 		leaveDropZone: function() {
 			this.hover = false;
 			let availableColors = ['red','pink','blue', 'green', 'yellow', 'indigo', 'purple'];
 			let color = availableColors[Math.floor(Math.random() * availableColors.length)]
 			this.colorClass = 'border-'+color+'-600'
-		}
+		},
+    enterDropZone: function() {
+      this.hover = true
+    }
 	}
 }
 </script>
--- src/ipcMain.js
+++ src/ipcMain.js
@@ -1,6 +1,41 @@
 const { ipcMain, session } = require('electron');
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
 
 ipcMain.on('logout', (event, arguments) => {
-	console.debug(session.defaultSession);
 	session.defaultSession.clearStorageData();
 })
+
+// Can be anything either a file or folder
+ipcMain.on('upload', async (event, filePath) => {
+  let isDirectory = fs.lstatSync(filePath).isDirectory();
+  let isFile = fs.lstatSync(filePath).isFile();
+
+  if (isDirectory) {
+    let files = fs.readdirSync(filePath);
+    for (let file of files) {
+      if (fs.lstatSync(filePath +'/'+ file).isFile()) {
+        upload(file);
+      }
+    }  } else if (isFile) {
+    upload(filePath);
+  }
+  
+})
+
+// Single file upload
+function upload(file) {
+  const form = new FormData();
+  let fileName = path.parse(file).name;
+  
+  let fileData = fs.createReadStream(file);
+  form.append('file', fileData, fileName)
+  
+  axios.post('https://pixeldrain.com/api/file', form, {
+    headers: form.getHeaders()
+  })
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
+}
